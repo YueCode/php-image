9f3ae0ee3745b90adba0cbab0b09f15f702fd735
Bestony <xiqingongzi@gmail.com>
2017-02-28 00:24:40 +0800

    加入Provider

---
+++ src/ImageProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Yuecode\Image;
+
+use Illuminate\Support\ServiceProvider;
+
+class ImageProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/config.php' => config_path('image.php'),
+        ]);
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('youtu',function (){
+            return new YouTu();
+        });
+    }
+}
---
+++ src/config.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'APPID'=>'', // 项目ID，可在https://console.qcloud.com/image/bucket获取
+    'SECRET_ID'=>'', // 可在 https://console.qcloud.com/image/project 获取
+    'SECRET_KEY'=>'', // 可在 https://console.qcloud.com/image/project 获取
+    'BUCKET'=>'' // 空间名称，可在https://console.qcloud.com/image/bucket获取
+];
